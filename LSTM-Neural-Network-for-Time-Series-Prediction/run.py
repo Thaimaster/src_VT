294cfa5f6575d39f6e0157b88dab970d934eba75
Nguyễn Đình Thái <57148163+Thaimaster@users.noreply.github.com>
2023-05-22 09:05:17 +0700

    Add files via upload

---
+++ LSTM-Neural-Network-for-Time-Series-Prediction/run.py
@@ -0,0 +1,102 @@
+__author__ = "Jakob Aungiers"
+__copyright__ = "Jakob Aungiers 2018"
+__version__ = "2.0.0"
+__license__ = "MIT"
+
+import os
+import json
+import time
+import math
+import argparse
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from core.data_processor import DataLoader
+from core.model import Model
+
+
+
+def plot_results(predicted_data, true_data):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+    plt.plot(predicted_data, label='Prediction')
+    plt.legend()
+    plt.show()
+
+
+def plot_results_multiple(predicted_data, true_data, prediction_len):
+    fig = plt.figure(facecolor='white')
+    ax = fig.add_subplot(111)
+    ax.plot(true_data, label='True Data')
+	# Pad the list of predictions to shift it in the graph to it's correct start
+    for i, data in enumerate(predicted_data):
+        padding = [None for p in range(i * prediction_len)]
+        plt.plot(padding + data, label='Prediction')
+        plt.legend()
+    plt.show()
+    plt.savefig("result.png")
+
+
+def main(args):
+
+    # Get config file
+    start = time.time()
+    configs = json.load(open(args.config_file, 'r'))
+    if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
+
+    data = DataLoader(
+        os.path.join('data', configs['data']['filename']),
+        configs['data']['train_test_split'],
+        configs['data']['columns']
+    )
+
+    model = Model()
+    model.build_model(configs)
+    x, y = data.get_train_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+    '''
+	# in-memory training
+	model.train(
+		x,
+		y,
+		epochs = configs['training']['epochs'],
+		batch_size = configs['training']['batch_size'],
+		save_dir = configs['model']['save_dir']
+	)
+	'''
+    # out-of memory generative training
+    steps_per_epoch = math.ceil((data.len_train - configs['data']['sequence_length']) / configs['training']['batch_size'])
+    model.train_generator(
+        data_gen=data.generate_train_batch(
+            seq_len=configs['data']['sequence_length'],
+            batch_size=configs['training']['batch_size'],
+            normalise=configs['data']['normalise']
+        ),
+        epochs=configs['training']['epochs'],
+        batch_size=configs['training']['batch_size'],
+        steps_per_epoch=steps_per_epoch,
+        save_dir=configs['model']['save_dir']
+    )
+
+    print("Time Training:", time.time()-start)
+    x_test, y_test = data.get_test_data(
+        seq_len=configs['data']['sequence_length'],
+        normalise=configs['data']['normalise']
+    )
+
+    predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'], configs['data']['sequence_length'])
+    # predictions = model.predict_sequence_full(x_test, configs['data']['sequence_length'])
+    # predictions = model.predict_point_by_point(x_test)
+
+    plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+    # plot_results(predictions, y_test)
+    print("Total Time :", time.time()-start)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Generator data for LSTM model")
+    parser.add_argument("config_file", type=str, help="config_file")
+    main(parser.parse_args())
